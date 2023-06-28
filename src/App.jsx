ae3cb3732a14f93146d6b5ceabf7e726b15e4394
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-29 01:43:21 +0200

    feat: setup react applciation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <h1>Home component</h1>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
